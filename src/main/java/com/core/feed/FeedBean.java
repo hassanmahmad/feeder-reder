a3a1378ea150e14b9cb899bf16ad0aabbb2fddfb
hassanmahmad <39093304+hassanmahmad@users.noreply.github.com>
2018-05-13 16:35:53 +0500

    Update FeedBean.java

--- src/main/java/com/core/feed/FeedBean.java
+++ src/main/java/com/core/feed/FeedBean.java
@@ -15,16 +15,24 @@ import javax.ejb.Stateless;
 public class FeedBean {
     
     private List<String> listIt = new ArrayList<>(); 
+    private static List<String> timeStamp = new ArrayList<>();
 
     public int calAdd(int a, int b)
     {
         return (a+b);
     }
     
-    public List<String> calAdList(String b)
+     public List<String> calAdList(String b)
     {
         listIt.add(b);
+        String ts = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+        timeStamp.add(ts);
         return listIt;
+    }
+    
+    public List<String> getTime()
+    {
+        return timeStamp;
     }
     
 }
